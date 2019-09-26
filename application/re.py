7a22448d0a3f1bb5e4d3fc0285f776fde4801a40
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-09-26 11:18:33 -0300

    Add edit_expression method

--- application/re.py
+++ application/re.py
@@ -1,7 +1,14 @@
-"""
-    Regular Expression explanation text
-"""
+"""Regular Expression explanation text"""
 
 class RE():
     def __init__(self, expression: str):
         self.expression = expression
+
+    """ Returns a DFA equivalent to this Regular Expression using the syntax
+        tree algorithm """
+    def convert_to_dfa(self):
+        #TODO implement
+        pass
+    
+    def edit_expression(self, new_expression: str):
+        self.expression = new_expression
